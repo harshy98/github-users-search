07210ade09f6cc3c18b9cd38abcb1d1fa8694be9
Harsh Diwakar <harsh_diwakar@Harshs-MacBook-Air.local>
2022-01-29 16:13:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./styles.css";
+import { useState, useEffect } from "react";
+function App({ user }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [userid, setUserid] = useState("");
+  const input = (event) => {
+    setUserid(event.target.value);
+  };
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userid}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, [userid]);
+  console.log(data);
+  if (loading) return "Please wait, loading...";
+  else {
+    return (
+      <div className="App">
+        <input type="text" onChange={input} />
+        <h1>Name:{data.name}</h1>
+        <p>Location:{data.location}</p>
+        <p>Followers:{data.followers}</p>
+        <p>Following:{data.following}</p>
+      </div>
+    );
+  }
 }
-
 export default App;
